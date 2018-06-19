5b95a0cb7fd22db26b6bc6d2385d6335fb6872ae
Liza Chalova <death_innocent_2@mail.ru>
2018-06-19 21:16:24 +0500

    zeidel method

--- slau.py
+++ slau.py
@@ -2,37 +2,18 @@ import numpy as np
 
 ITERATION_LIMIT = 1000
 
-# Матрица констант
-A = np.array([[0.07, -0.08, 0.11, -0.18],
-              [0.18, 0.52, 0.0, 0.21],
-              [0.13, 0.31, 0.0, -0.21],
-              [0.08, -0.0, -0.33, 0.28]])
-# Вектор свободных элементов
-b = np.array([-0.51, 1.17, -1.02, -0.28])
 
-# Вывод слау
-print("System:")
-for i in range(A.shape[0]):
-    row = ["{}*x{}".format(A[i, j], j + 1) for j in range(A.shape[1])]
-    row1 = "x{} =".format(i+1)
-    print(row1, " + ".join(row), b[i])
-print()
-
-# Подсчет норм и проверка на сходимость
-norm_1 = np.linalg.norm(A, 1)
-norm_2 = np.linalg.norm(A, np.inf)
-norm_3 = np.linalg.norm(A, 'fro')
-
-if norm_1 < 1 or norm_2 < 1 or norm_3 < 1:
+def jacobi(a, b):
 
     x = np.zeros_like(b)
+
     for it_count in range(ITERATION_LIMIT):
-        print("Current solution:", x)
+        print("{0}-th iteration: {1}".format(it_count, x))
         x_new = np.zeros_like(x)
 
-        for i in range(A.shape[0]):
-            su = np.dot(A[i], x[:])
-            x_new[i] = b[i] + su
+        for n in range(a.shape[0]):
+            su = np.dot(a[n], x[:])
+            x_new[n] = b[n] + su
 
         # Принцип Рунге - точность = 10**-5
         if np.allclose(x, x_new, atol=1e-5, rtol=0.):
@@ -43,6 +24,54 @@ if norm_1 < 1 or norm_2 < 1 or norm_3 < 1:
     print("Solution:")
     print(x)
 
+
+def zeidel(a, b):
+
+    x = np.zeros_like(b)
+
+    for it_count in range(ITERATION_LIMIT):
+        print("{0}-th iteration: {1}".format(it_count, x))
+        x_new = np.zeros_like(x)
+
+        for n in range(a.shape[0]):
+            s1 = np.dot(a[n, :n], x_new[:n])
+            s2 = np.dot(a[n, n:], x[n:])
+            x_new[n] = b[n] + s1 + s2
+
+        if np.allclose(x, x_new, atol=1e-5, rtol=0.):
+            break
+
+        x = x_new
+
+    print("Solution:")
+    print(x)
+
+
+# Матрица констант
+a_matrix = np.array([[0.07, -0.08, 0.11, -0.18],
+                    [0.18, 0.52, 0.0, 0.21],
+                    [0.13, 0.31, 0.0, -0.21],
+                    [0.08, -0.0, -0.33, 0.28]])
+# Вектор свободных элементов
+b_el = np.array([-0.51, 1.17, -1.02, -0.28])
+
+# Вывод слау
+print("System:")
+for i in range(a_matrix.shape[0]):
+    row = ["{}*x{}".format(a_matrix[i, j], j + 1)
+           for j in range(a_matrix.shape[1])]
+    row1 = "x{} =".format(i+1)
+    print(row1, " + ".join(row), b_el[i])
+print()
+
+# Подсчет норм и проверка на сходимость
+norm_1 = np.linalg.norm(a_matrix, 1)
+norm_2 = np.linalg.norm(a_matrix, np.inf)
+norm_3 = np.linalg.norm(a_matrix, 'fro')
+
+if norm_1 < 1 or norm_2 < 1 or norm_3 < 1:
+    jacobi(a_matrix, b_el)
+    zeidel(a_matrix, b_el)
 else:
     print('The iteration process does not converge.\n')
     print("1-th norm = {0}\n2-th norm = {1}\n3-th norm = {2}\n".format(norm_1, norm_2, norm_3))
